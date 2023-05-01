698e8df591c18685c7f5679f7318322d6c13b1ff
Kent-cpu <74884403+Kent-cpu@users.noreply.github.com>
2023-05-01 13:42:10 +0700

    Added model user

---
+++ src/main/kotlin/com/example/automotive_api/models/User.kt
@@ -0,0 +1,31 @@
+package com.example.automotive_api.models
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Entity
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int = 0
+
+    @Column
+    var name = ""
+
+    @Column(unique = true)
+    var email = ""
+
+    @Column
+    var password = ""
+        @JsonIgnore
+        get() = field
+        set(value) {
+            val passwordEncoder = BCryptPasswordEncoder()
+            field = passwordEncoder.encode(value)
+        }
+
+    fun comparePassword(password: String): Boolean {
+        return BCryptPasswordEncoder().matches(password, this.password)
+    }
+}
