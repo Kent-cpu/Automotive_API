b0734562c89db2a6255fc495e55c6f63b2dff043
Kent-cpu <74884403+Kent-cpu@users.noreply.github.com>
2023-05-01 13:37:49 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/automotive_api/AutomotiveApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.automotive_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutomotiveApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutomotiveApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/automotive_api/AutomotiveApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.automotive_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutomotiveApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
